68b1ff144099585607d1458d54c0a4497296f4c1
Vadim Vilkhivskiy <vadim.v.valki@gmail.com>
2022-07-21 23:38:19 +0300

    added transition between pages (Router)

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,42 +1,22 @@
-import { useState } from 'react';
-import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundaru from '../errorBoundary/ErrorBoundary';
-import AppBanner from '../appBanner/AppBanner';
-import ComicsList from '../comicsList/ComicsList';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
-import decoration from '../../resources/img/vision.png';
+import {MainPage, ComicsPage} from '../pages';
+import AppHeader from "../appHeader/AppHeader";
 
 const App = () => {
-
-    const [selectedChar, setChar] = useState(null)
-
-    const onCharSelected = (id) => {
-        setChar(id);
-    }
-
+    
     return (
-        <div className="app">
-            <AppHeader />
-            <main>
-                {/* <ErrorBoundaru>
-                    <RandomChar />
-                </ErrorBoundaru>
-                <div className="char__content">
-                    <ErrorBoundaru>
-                        <CharList onCharSelected={onCharSelected} />
-                    </ErrorBoundaru>
-                    <ErrorBoundaru>
-                        <CharInfo charId={selectedChar} />
-                    </ErrorBoundaru>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision" /> */}
-                <AppBanner/>
-                <ComicsList/>
-            </main>
-        </div>
+        <Router>
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <Routes>
+                        <Route path="/" element={<MainPage/>}/>
+                        <Route path="/comics" element={<ComicsPage/>}/>
+                    </Routes>
+                </main>
+            </div>
+        </Router>
     )
 }
 
